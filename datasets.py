139a88030f6b7cc00954b244bf4f951e25499373
Jack <45013505+Skullbaby@users.noreply.github.com>
2025-12-14 07:51:38 -0600

    Create datasets.py

---
+++ datasets.py
@@ -0,0 +1,7 @@
+DATASETS = {
+    "inc5000_2019": {
+        "dataset_id": "inc5000_2019",
+        "source_uri": "/Home/Documents/Data/Inc/INC 5000 Companies 2019.csv",
+        "output_dir": "/Home/Documents/Data/Inc/output/inc5000_2019",
+    }
+}
