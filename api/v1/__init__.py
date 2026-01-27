88c2f20bccff93138381e27ae511dafac3d6adec
Jack <45013505+Skullbaby@users.noreply.github.com>
2026-01-27 09:34:11 -0500

    Update __init__.py

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -7,6 +7,7 @@ from .leases import router as leases_router
 from .results import router as results_router
 from .read import router as read_router
 from .events import router as events_router
+from .reclamation.agents import router as reclamation_agents_router
 
 router = APIRouter()
 
@@ -18,4 +19,5 @@ router.include_router(results_router, tags=["v1/results"])
 # Read endpoints + realtime
 router.include_router(read_router, tags=["v1/read"])
 router.include_router(events_router, tags=["v1/events"])
+router.include_router(reclamation_agents_router, tags=["v1/reclamation"])
 
